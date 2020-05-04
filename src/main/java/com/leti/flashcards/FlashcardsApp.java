2701534bc3589b6ead5c0bbf82be7779b9867bcb
Mariia_Zemskova <lostforstupid@gmail.com>
2020-05-04 22:25:25 +0300

    basics

---
+++ src/main/java/com/leti/flashcards/FlashcardsApp.java
@@ -0,0 +1,10 @@
+package com.leti.flashcards;
+
+import org.springframework.boot.SpringApplication;
+
+public class FlashcardsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcardsApp.class, args);
+    }
+}
